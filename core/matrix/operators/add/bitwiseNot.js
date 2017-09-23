ad7d5ed9b1730f10758d7f004045dcc315350bb0
Steve Lam <slam@akshonesports.com>
2017-09-22 21:25:17 -0700

    fixed `bitwiseNot` operator

--- core/matrix/operators/add/bitwiseNot.js
+++ core/matrix/operators/add/bitwiseNot.js
@@ -2,7 +2,6 @@ var core = require('../../../');
 
 core.Matrix.prototype.bitwiseNot = function() {
   var out = new core.Matrix();
-  var args = Array.from(arguments);
-  core.bitwiseNot.apply(global, args.slice(0, 1).concat(out, args.slice(1)));
+  core.bitwiseNot.apply(global, [this, out].concat(Array.from(arguments)));
   return out;
 };
