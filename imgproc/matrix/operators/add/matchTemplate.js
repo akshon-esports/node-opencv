98f0e098c68edf200ca5aa495a6cd6dea93456b3
Steve Lam <slam@akshonesports.com>
2017-09-22 21:23:00 -0700

    fixed `matchTemplate` operator

--- imgproc/matrix/operators/add/matchTemplate.js
+++ imgproc/matrix/operators/add/matchTemplate.js
@@ -6,6 +6,6 @@ core.Matrix.prototype.matchTemplate = function() {
   var args = Array.from(arguments);
   args.splice(1, 0, out);
   args.splice(0, 0, this);
-  core.matchTemplate.apply(global, args);
+  imgproc.matchTemplate.apply(global, args);
   return out;
 };
