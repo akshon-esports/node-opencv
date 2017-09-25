9549fd06ecb210427369a4c4557f03c0fdb345d5
Steve Lam <slam@akshonesports.com>
2017-09-25 12:50:00 -0700

    fixed `Matrix.addWeighted`

--- core/index.js
+++ core/index.js
@@ -2,7 +2,7 @@ var core = require(require('../binary')() + '/core.node');
 
 core.Matrix.addWeighted = function() {
   var out = new core.Matrix();
-  var args = Array.from();
+  var args = Array.from(arguments);
   if (args.length >= 5) {
     // not enough arguments, just let it fail
     args.splice(5, 0, out);
