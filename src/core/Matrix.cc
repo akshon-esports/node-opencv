348c45fc7aff0f50edd8e3f04cebf51eb6502f18
Steve Lam <slam@akshonesports.com>
2017-10-02 11:20:30 -0700

    fixed `Matrix::create`

--- src/core/Matrix.cc
+++ src/core/Matrix.cc
@@ -413,9 +413,11 @@ NAN_METHOD(Matrix::Create) {
       cv::Size size = Size::RawSize(1, &val);
       ASSERT_INT_FROM_ARGS(type, 1);
       TRY_CATCH_THROW_OPENCV(self->mat.create(size, info[1]->NumberValue()));
+      return;
     } catch (char const*) {}
   } else if (info.Length() == 3 && info[0]->IsNumber() && info[1]->IsNumber() && info[2]->IsNumber()) {
     TRY_CATCH_THROW_OPENCV(self->mat.create(info[0]->NumberValue(), info[1]->NumberValue(), info[2]->NumberValue()));
+    return;
   }
 
   return Nan::ThrowError(ERROR_INVALID_ARGUMENTS);
